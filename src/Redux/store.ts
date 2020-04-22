40665535dacb7f7dbf587084979fa35bdc66b56e
fleshascs <35168446+fleshascs@users.noreply.github.com>
2020-04-22 19:29:09 +0300

    prepare to deploy

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,18 +1,26 @@
 // #region Global Imports
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 // #endregion Global Imports
 
 // #region Local Imports
 import Reducers from "./Reducers";
 // #endregion Local Imports
 
+const bindMiddleware = (middleware: any) => {
+    if (process.env.NODE_ENV !== "production") {
+        const {
+            composeWithDevTools,
+        } = require("redux-devtools-extension/developmentOnly");
+        return composeWithDevTools(applyMiddleware(...middleware));
+    }
+    return applyMiddleware(...middleware);
+};
+
 export const makeStore = (initialState: {}) => {
     return createStore(
         Reducers,
         initialState,
-        composeWithDevTools(applyMiddleware(thunkMiddleware))
+        bindMiddleware([thunkMiddleware])
     );
 };
